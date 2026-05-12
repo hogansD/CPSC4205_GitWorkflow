649c3995219643e8c6b6485700a5b2408d549749
hogansD <diamond.a.hogans@gmail.com>
2026-05-12 11:26:21 -0400

    Add email address field to student profile also my email in the example was certainly a jump scare

--- StudentInfo.java
+++ StudentInfo.java
@@ -4,6 +4,7 @@ public class StudentInfo {
         String name = "John Doe";
         String major = "Information Technology";
         int graduationYear = 2025;
+        String email = "hogans_diamond@colstate.view.usg.edu";
 
         System.out.println("==========================================");
         System.out.println("       STUDENT INFORMATION CARD");
@@ -11,6 +12,7 @@ public class StudentInfo {
         System.out.println("Name: " + name);
         System.out.println("Major: " + major);
         System.out.println("Graduation Year: " + graduationYear);
+        System.out.println("Email: " + email);
         System.out.println("==========================================");
     }
 }
